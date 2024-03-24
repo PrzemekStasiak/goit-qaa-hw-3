8c19de0e07809acb6ed8909e51eaeb2c92a5603f
PrzemekStasiak <140719984+PrzemekStasiak@users.noreply.github.com>
2024-03-24 12:11:55 +0100

    cosmetic upgrade

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const fruits = ["Jabłko", "Banan", "Pomarańcza", "Winogrona", "Arbuz"];
 console.log(fruits)
 
 const numbers = [2, 4, 7, 12, 54, 2, 4, 7, 0, 99]
-for (let i =0; i<numbers.length; i++){
+for (let i = 0; i<numbers.length; i++){
     if (numbers[i] > 5);
     console.log(numbers[i]);
 } 
